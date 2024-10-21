9eb8acf99915c5724ab65b8ffcdd442d07a0c6fc
Jackie.yyqmbp <jackieyqi@gmail.com>
2024-10-21 15:46:43 +0800

    update

--- amfconsumer.py
+++ amfconsumer.py
@@ -5,13 +5,12 @@ import asyncio
 import logging
 import sys
 
-from exts import amf_queue, queue_conn, amf_queue_plot
+from exts import amf_queue, queue_conn
 from msgqueue import deal_msg
 
 logger = logging.getLogger("amfconsumer")
 
 RESTART = False
-RESTART_PLOT = False
 
 
 async def consumer():
@@ -41,33 +40,9 @@ async def consumer():
         # print(type(value), value.body)
         await deal_msg(value.body)
 
-    mq_plot = queue_conn.SimpleQueue(amf_queue_plot)
-    global RESTART_PLOT
-    while 1:
-        if RESTART_PLOT:
-            logger.info("Consumer will quit")
-            sys.exit(0)
-
-        value = None
-        try:
-            value = mq_plot.get(block=False, timeout=1)
-            value.ack()
-        except mq_plot.Empty:
-            pass
-        except:
-            queue_conn.connect()
-            mq_plot = queue_conn.SimpleQueue(amf_queue_plot)
-
-        if not value:
-            await asyncio.sleep(0.5)
-            continue
-        await deal_msg(value.body)
-
 
 def deal_signal(*args, **kwargs):
     global RESTART
     RESTART = True
 
-    global RESTART_PLOT
-    RESTART = True
     logger.info("Consumer restart")
--- msgqueue/queue.py
+++ msgqueue/queue.py
@@ -4,7 +4,7 @@
 import logging
 import traceback
 import ujson as json
-from exts import queue_conn, amf_queue, amf_queue_plot
+from exts import queue_conn, amf_queue
 
 
 logger = logging.getLogger("MQ")
@@ -13,15 +13,6 @@ logger = logging.getLogger("MQ")
 async def push(value):
     try:
         with queue_conn.SimpleQueue(amf_queue) as queue:
-            queue.put(json.dumps(value))
-    except BaseException as e:
-        logger.error("{}".format(traceback.format_exc()))
-    return True
-
-
-async def push_plot(value):
-    try:
-        with queue_conn.SimpleQueue(amf_queue_plot) as queue:
             queue.put(json.dumps(value))
     except BaseException as e:
         logger.error("{}".format(traceback.format_exc()))
